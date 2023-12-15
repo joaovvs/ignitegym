71b8d111894e6410e20bc90b4bebd37f80eb62e0
Joao Vinicius <joaovvs@gmail.com>
2023-12-15 20:08:05 -0300

    style: component library - theme customization

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({  Roboto_400Regular, Roboto_700Bold  })
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}}>
-      <StatusBar
+    <NativeBaseProvider theme={THEME}>
+    <StatusBar
           backgroundColor='transparent'
           style='light' 
           translucent
         />
       
-      { fontsLoaded ? <Text>Hello World!</Text>: <View/>}
-    </View>
+      { fontsLoaded ? <View/>: <Loading/>}
+    </NativeBaseProvider>
   );
 }
 
