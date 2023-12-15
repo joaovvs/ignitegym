5651a241bbadfc295f8041668682e96387d58262
Joao Vinicius <joaovvs@gmail.com>
2023-12-15 20:47:23 -0300

    component input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,26 +1,34 @@
-import { VStack, Image, Text, Center } from 'native-base'
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
-import LogoSvg from '@assets/logo.svg'
-import BackgroundImg from '@assets/background.png';
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 
+import { Input } from "@components/Input";
 
-export function SignIn(){
-    return(
-        <VStack flex={1} bg='gray.700'>
-            <Image 
-                source={BackgroundImg}
-                alt='Pessoas treinando na academia'
-                resizeMode='contain'
-                position='absolute'
-            />
-            <Center my={32}>
-            <LogoSvg/>
+export function SignIn() {
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={BackgroundImg}
+        alt="Pessoas treinando na academia"
+        resizeMode="contain"
+        position="absolute"
+      />
+      <Center my={32}>
+        <LogoSvg />
 
-            <Text color={'gray.100'} fontSize={'sm'}>
-                Treine sua mente e o seu corpo
-            </Text>
-            </Center>
-        </VStack>
-    
-    );
-};
+        <Text color={"gray.100"} fontSize={"sm"}>
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+      <Center>
+        <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
+          Acesse sua Conta
+        </Heading>
+
+        <Input placeholder="E-mail"/>
+        <Input placeholder="Senha"/>
+      </Center>
+    </VStack>
+  );
+}
