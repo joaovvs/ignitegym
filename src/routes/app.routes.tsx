2ac7f0ca976f20089672bddc1c612499198446b4
Joao Vinicius <joaovvs@gmail.com>
2023-12-16 14:37:28 -0300

    create appRoutes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,30 @@
+import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+
+import { Home } from "@screens/Home";
+import { Exercise } from "@screens/Exercise";
+import { Profile } from "@screens/Profile";
+import { History } from "@screens/History";
+
+type AppRoutes = {
+    home: undefined;
+    profile: undefined;
+    history: undefined;
+    exercise: undefined;
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false}}>
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
+}
