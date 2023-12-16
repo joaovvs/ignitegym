e93dc5a3c3b852e2e03af78cafcc10bda09a02e0
Joao Vinicius <joaovvs@gmail.com>
2023-12-16 16:14:24 -0300

    group component and home screen init

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,49 @@
+import { useState } from 'react';
+
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack, Text } from 'native-base';
+import { VStack, FlatList, HStack, Heading, Text } from 'native-base';
+
+import { Group } from '@components/Group';
 
 export function Home(){
+    const [groups, setGroups] = useState(['Costas', 'Bíceps','Tríceps','Ombro']);
+    const [groupSelected, setGroupSelected] = useState('Costas');
+
     return(
         <VStack flex={1}>
             <HomeHeader/>
+
+            
+ 
+                <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <Group 
+                        name={item} 
+                        isActive={groupSelected===item}
+                        onPress={() => setGroupSelected(item)}/>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{px: 8}}
+                marginY={10}
+                maxHeight={10}
+                />
+            <VStack flex={1} px={8}>
+                <HStack justifyContent={'space-between'} marginBottom={5}>
+                    <Heading color={'gray.200'} fontSize={'md'}>
+                        Exercícios
+                    </Heading>
+
+                    <Text color={'gray.200'} fontSize={'sm'}>4</Text>
+                </HStack>
+
+            </VStack>
+            
+
+            
+            
         </VStack>
     );
 }
