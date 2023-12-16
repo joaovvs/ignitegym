f5ce75845ec7f34c1a84cb251574e36a6a1da8b4
Joao Vinicius <joaovvs@gmail.com>
2023-12-16 11:38:28 -0300

    screen SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,7 +6,7 @@ import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
     <VStack flex={1} bg="gray.700" px={10} pb={16}>
@@ -25,22 +25,21 @@ export function SignIn() {
       </Center>
       <Center>
         <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
-          Acesse sua Conta
+          Crie sua Conta
         </Heading>
 
+        <Input placeholder="Nome"/>
         <Input placeholder="E-mail" keyboardType="email-address" autoCapitalize="none"/>
         <Input placeholder="Senha" secureTextEntry />
 
-        <Button title="Acessar" />
+        <Button title="Criar e acessar" />
       </Center>
 
-      <Center mt={24}>
-        <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-          Ainda não tem acesso?
-        </Text>
 
-        <Button title="Criar conta" variant="outline" />
-      </Center>
+        <Button 
+        title="Voltar para o login" 
+        variant="outline" 
+        mt={24}/>
     </VStack>
     </ScrollView>
   );
