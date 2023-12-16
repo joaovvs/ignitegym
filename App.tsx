db19604923037e5921da4ea06ae09e773fefe79f
Joao Vinicius <joaovvs@gmail.com>
2023-12-16 12:30:41 -0300

    add authRoutes

--- App.tsx
+++ App.tsx
@@ -5,8 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
-import { SignUp } from '@screens/SignUp';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({  Roboto_400Regular, Roboto_700Bold  })
@@ -18,7 +17,7 @@ export default function App() {
           translucent
         />
       
-      { fontsLoaded ? <SignUp/>: <Loading/>}
+      { fontsLoaded ? <Routes/>: <Loading/>}
     </NativeBaseProvider>
   );
 }
