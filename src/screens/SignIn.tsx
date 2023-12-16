76c3b1799762060cb5c7f0ca666b8c41f2ee679e
Joao Vinicius <joaovvs@gmail.com>
2023-12-16 11:32:39 -0300

    screen SignIn done

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
@@ -8,14 +8,15 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
+    <VStack flex={1} bg="gray.700" px={10} pb={16}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando na academia"
         resizeMode="contain"
         position="absolute"
       />
-      <Center my={32}>
+      <Center my={24}>
         <LogoSvg />
 
         <Text color={"gray.100"} fontSize={"sm"}>
@@ -27,13 +28,20 @@ export function SignIn() {
           Acesse sua Conta
         </Heading>
 
-        <Input placeholder="E-mail"/>
-        <Input placeholder="Senha" type="password"/>
+        <Input placeholder="E-mail" />
+        <Input placeholder="Senha" type="password" />
 
-        <Button title='Acessar'/>
+        <Button title="Acessar" />
+      </Center>
 
-        <Button title='Criar conta' variant='outline'/>
+      <Center mt={24}>
+        <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+          Ainda não tem acesso?
+        </Text>
+
+        <Button title="Criar conta" variant="outline" />
       </Center>
     </VStack>
+    </ScrollView>
   );
 }
